5b6f6b84e1218de6d5f045536ce978037294fadd
Udhay Adithya <udhayxd@gmail.com>
2024-03-28 22:07:29 +0530

    Update Exam schedule

--- src/exam_schedule.py
+++ src/exam_schedule.py
@@ -17,7 +17,28 @@ def exam_schedule(session,username,csrf_token):
           }
     html=session.post(GET_EXAM_SCHEDULE_URL,data=data,headers=USER_AGENT).text
     soup = BeautifulSoup(html, "html.parser")
-    user_data = soup.find_all('td')
-    for i in user_data:
-        with open('exam_schedule.json', 'a') as json_file:
-            json.dump(user_data, json_file, indent=4)
+    values = soup.find_all('td')[15:]
+
+    result = {}
+    subject_data = {}
+    index = 1
+
+    header = ['S.No.', 'Course Code', 'Course Title', 'Course Type', 'Class ID', 'Slot', 'Exam Date', 
+            'Exam Session', 'Reporting Time', 'Exam Time', 'Venue', 'Seat Location', 'Seat No.']
+
+    for i, value in enumerate(values, start=1):
+        if i % 13 == 1:
+            if subject_data:
+                  result[index] = subject_data
+                  subject_data = {}
+                  index += 1
+        key = header[i % 13 - 1]
+        subject_data[key] = value.get_text().strip()
+
+      # Adding the last subject data
+    if subject_data:
+      result[index] = subject_data
+
+      #Write the data into a JSON file
+    with open('exam_scledule.json', 'w') as json_file:
+        json.dump(result, json_file, indent=4)
