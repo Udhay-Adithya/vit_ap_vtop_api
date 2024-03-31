e83b88bfdd4c70e38bcaf243a33a92159a96e920
Udhay Adithya <udhayxd@gmail.com>
2024-03-31 14:22:37 +0530

    modified:   Procfile

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+def username():
+    username = input("Enter your username : ")
+    password = input("Enter your Password : ")
+
 if __name__ == '__main__':
     app.run(debug=True)
