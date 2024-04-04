b346a39b0414d19b16e05e9ced11e2c512118cb6
Udhay Adithya <udhayxd@gmail.com>
2024-04-04 16:25:16 +0530

    changed captcha.py

--- src/captcha.py
+++ src/captcha.py
@@ -1,29 +1,28 @@
 import requests
-from .constants import VTOP_URL, VTOP_LOGIN_URL,VTOP_PRELOGIN_URL, USER_AGENT
+from .constants import VTOP_LOGIN_URL,VTOP_PRELOGIN_URL, USER_AGENT
 from .tools import find_captcha,find_csrf
 
 MAX_RETRIES=3
 session = requests.Session()
 def fetch_and_display_captcha(session, retries=MAX_RETRIES):
-    try:
-        csrf_token = find_csrf(session.get(VTOP_URL, headers=USER_AGENT).text)
-        data = {'_csrf': csrf_token, 'flag': 'VTOP'}
-        response = session.post(VTOP_PRELOGIN_URL, data=data, headers=USER_AGENT)
-        response.raise_for_status()
-        if response.ok:
-            print("Pre-login successful")
-        else:
-            print("Pre-login failed")
-    except requests.RequestException as e:
-        print("Pre-login request failed:", e)
-    
+
     try:
         html = session.get(VTOP_LOGIN_URL, headers=USER_AGENT).text
         base64_code = find_captcha(html)
         print(base64_code)
-        if base64_code:
+        if base64_code != 'Null':
             return base64_code
+        elif retries > 0:
+                print(f"Retrying... {retries} retries left.")
+                return fetch_and_display_captcha(session, retries - 1)
         else:
-            return 404
+                print("Maximum retries reached. Captcha not found.")
+                return None
     except requests.RequestException as e:
         print("Failed to fetch captcha:", e)
+        if retries > 0:
+            print(f"Retrying... {retries} attempts left.")
+            return fetch_and_display_captcha(session, retries=retries-1)
+        else:
+            print("Maximum retries reached. Aborting.")
+            return False
