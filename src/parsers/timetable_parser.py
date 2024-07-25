0853e178e5c954a18a5410caa40b45d5330c8731
Udhay Adithya <udhayxd@gmail.com>
2024-07-25 17:39:27 +0530

    fixed a bug where consequtive classes with same timeslot  are not visible

--- src/parsers/timetable_parser.py
+++ src/parsers/timetable_parser.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup
 
-time_table_data = {'Tuesday':{},'Wednesday':{},'Thursday':{},'Friday':{},'Saturday':{}}
+time_table_data = {
+    'Tuesday': [],
+    'Wednesday': [],
+    'Thursday': [],
+    'Friday': [],
+    'Saturday': []
+}
 theory_timings=['DAY','TYPE','08:00 - 08:50', '09:00 - 09:50', '10:00 - 10:50', '10:00 - 10:50', '11:00 - 11:50', '11:00 - 11:50', '12:00 - 12:50','12:00 - 12:50', '13:00 - 13:50', 'Lunch', '14:00 - 14:50', '14:00 - 14:50', '15:00 - 15:50', '16:00 - 16:50', '16:00 - 16:50', '17:00 - 17:50', '17:00 - 17:50', '18:00 - 18:50', '19:00 - 19:50']
-lab_timings=['TYPE','08:00 - 08:50','09:00 - 09:50', '09:50 - 10:40', '11:00 - 11:50', '11:00 - 11:50', '11:50 - 12:40', '11:50 - 12:40', '12:50 - 13:30', 'Lunch', '14:00 - 14:50', '14:00 - 14:50', '14:50 - 15:40','16:00 - 16:50','16:00 - 16:50', '16:50 - 17:40', '16:50 - 17:40', '18:00 - 18:50', '18:50 - 19:30']
+lab_timings=['TYPE','08:00 - 08:50','09:00 - 09:50', '09:50 - 10:40','09:50 - 10:40', '11:00 - 11:50', '11:00 - 11:50', '11:50 - 12:40', '11:50 - 12:40', '12:50 - 13:30', 'Lunch', '14:00 - 14:50', '14:00 - 14:50', '14:50 - 15:40','16:00 - 16:50','16:00 - 16:50', '16:50 - 17:40', '16:50 - 17:40', '18:00 - 18:50', '18:50 - 19:30']
 lst_table=[]
 
 def get_course_info(html):
@@ -47,25 +53,26 @@ def get_course_info(html):
 
 
 def update_timetable_with_course_info(timetable_data, courses_list):
-    for day, timeslots in timetable_data.items():
-        for timeslot, course_info in timeslots.items():
-            # Extract the course code from the course_info string
-            course_code = course_info.split('-')[1]
-			#Extracting venue from the course_info string
-            course_venue = str(course_info.split('-')[3]+'-'+course_info.split('-')[4])
-			#Course type
-            course_type= course_info.split('-')[2]
-            # If the course code is in the courses_list, replace the course_info string with the course name and venue
-            for course in courses_list:
-                if course_code in course:
-                    if course_venue == str(course[course_code]['venue'].split('-')[0]+'-'+course[course_code]['venue'].split('-')[1]):
-                        timetable_data[day][timeslot] = {
-                            "course_name": course[course_code]["course_name"],
-                            "venue": course_venue,
-                            "course_code": course_code,
-                            "course_type": course_type
-                        }
-    return timetable_data
+	for day, timeslots in timetable_data.items():
+		for slot in range(0,len(timeslots)):
+			for timeslot, course_info in timeslots[slot].items():
+				# Extract the course code from the course_info string
+				course_code = course_info.split('-')[1]
+				#Extracting venue from the course_info string
+				course_venue = str(course_info.split('-')[3]+'-'+course_info.split('-')[4])
+				#Course type
+				course_type= course_info.split('-')[2]
+				for course in courses_list:
+					if course_code in course:
+						if course_venue == str(course[course_code]['venue'].split('-')[0]+'-'+course[course_code]['venue'].split('-')[1]):
+							timetable_data[day][slot][timeslot] = {
+								"course_name": course[course_code]["course_name"],
+								"venue": course[course_code]['venue'],
+								"course_code": course_code,
+								"course_type": course_type
+							}
+	print(time_table_data)
+	return timetable_data
                 
 
 
@@ -83,23 +90,19 @@ def parse_time_table(html):
 			lines = tr_string.split('\n')
 			lines = list(filter(None, lines))
 			lst_table.append(lines)
-		
 		#To access theory classes 	
 		for day,line in zip(['Tuesday','Wednesday','Thursday','Friday','Saturday'],lst_table[::2]):
 			for j in range(min(len(line), len(theory_timings))):
 				if len(line[j])>8 and line[j] != 'CLUBS/ECS':
-					time_table_data[day][theory_timings[j]] = line[j]
+					time_table_data[day].append({theory_timings[j] : line[j]})
 		#Labs
-		for day,line in zip(['Tuesday','Wednesday','Thursday','Friday','Saturday'],lst_table[1::2]):
+		for day, line in zip(['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], lst_table[1::2]):
 			for j in range(min(len(line), len(lab_timings))):
-				if len(line[j])>8:
-					time_table_data[day][lab_timings[j]] = line[j]
-
-		for day in time_table_data:
-			for time in list(time_table_data[day]):
-				value=time_table_data[day][time]
-				if len(value)<8:
-					del time_table_data[day][time]
+				if line[j] not in {'-', 'Lunch', ''}:
+					time_slot = lab_timings[j]
+					time_table_data[day].append({time_slot: line[j]})
+		for day, sessions in time_table_data.items():
+			time_table_data[day] = [session for session in sessions if all(len(value) >= 8 for value in session.values())]
 		return update_timetable_with_course_info(time_table_data,get_course_info(html))
 	else:														
 		return "Unable to find timetable."
