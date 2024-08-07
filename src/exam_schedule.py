33ae994b60fa57a00612ff9d4b0d8534e87c11d6
Udhay Adithya <udhayxd@gmail.com>
2024-08-07 23:17:26 +0530

    Fix bug with incorrect return type

--- src/exam_schedule.py
+++ src/exam_schedule.py
@@ -2,18 +2,19 @@ from .constants import *
 import time
 from .parsers import exam_schedule_parser
 
-def get_exam_schedule(session,username,semesterSubId,csrf_token):
-    data={'verifyMenu':'true',
-          'authorizedID':username,
-          '_csrf':csrf_token,
-          'nocache':int(round(time.time() * 1000))}
-    session.post(EXAM_SCHEDULE_URL,data=data,headers=HEADERS)
-    data={'authorizedID':username,
-          'semesterSubId':semesterSubId,
-          '_csrf':csrf_token
-          }
-    html=session.post(GET_EXAM_SCHEDULE_URL,data=data,headers=HEADERS)
-    if html:
-        return exam_schedule_parser.parse_exam_schedule(html.content)
-    else :
-        return "Unable to find exam schedule"
+
+def get_exam_schedule(session, username: str, semesterSubId: str, csrf_token) -> dict:
+    data = {
+        "verifyMenu": "true",
+        "authorizedID": username,
+        "_csrf": csrf_token,
+        "nocache": int(round(time.time() * 1000)),
+    }
+    session.post(EXAM_SCHEDULE_URL, data=data, headers=HEADERS)
+    data = {
+        "authorizedID": username,
+        "semesterSubId": semesterSubId,
+        "_csrf": csrf_token,
+    }
+    html = session.post(GET_EXAM_SCHEDULE_URL, data=data, headers=HEADERS)
+    return exam_schedule_parser.parse_exam_schedule(html.content)
--- src/parsers/exam_schedule_parser.py
+++ src/parsers/exam_schedule_parser.py
@@ -1,19 +1,19 @@
 from bs4 import BeautifulSoup
 
-def parse_exam_schedule(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    schedule_table = soup.find('table')
+
+def parse_exam_schedule(html: str) -> dict:
+    soup = BeautifulSoup(html, "html.parser")
+    schedule_table = soup.find("table")
 
     if not schedule_table:
-        print("Error: Could not find the schedule table in the HTML.")
-        return {"Error: Could not find the schedule table in the HTML."}
+        return {"error ": "No timetable found"}
 
-    rows = schedule_table.find_all('tr')
+    rows = schedule_table.find_all("tr")
     exam_schedule = {}
     current_exam_type = None
 
     for row in rows:
-        cells = row.find_all('td')
+        cells = row.find_all("td")
         values = [cell.text.strip() for cell in cells]
 
         # Skip rows that don't contain enough values or are headers
@@ -37,10 +37,10 @@ def parse_exam_schedule(html):
             "exam_time": values[9],
             "venue": values[10],
             "seat_location": values[11],
-            "seat_number": values[12]
+            "seat_number": values[12],
         }
 
         # Add the entry to the dictionary under the current exam type
         exam_schedule[current_exam_type][values[0]] = exam_entry
-        
+
     return exam_schedule
